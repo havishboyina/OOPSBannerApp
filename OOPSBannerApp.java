0e127d7b15508e5fe249f46e0eea262782ecae12
havish boyina <havishboyina1@gmail.com>
2026-02-22 20:19:20 +0530

    Create OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp - Prints OOPS banner for Use Case 1.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    /**
+     * Application entry point.
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
